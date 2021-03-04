baceda93319408230b7c8f69e7f8b48ecb4e89e7
chris westerduin <chriswesterduin@outlook.com>
2021-03-04 19:45:11 +0000

    init Pet

---
+++ index.js
@@ -0,0 +1,10 @@
+let myPet
+class Pet{
+    constructor(name, hungerLevel=10, happinessLevel=0){
+        this.name = name;
+        this.hungerLevel = hungerLevel;
+        this.happinessLevel = happinessLevel;
+    }
+}
+
+
